385d1953667d43a152eedfaae6371634b9595ca9
Tobias Kahan <tobyk100@gmail.com>
2013-04-26 14:22:14 -0700

    update convertPhantom

--- opendatakit.collect2/form-files/convertPhantom.js
+++ opendatakit.collect2/form-files/convertPhantom.js
@@ -1,27 +1,4 @@
-//load("js-xlsx/jszip.js", "js-xlsx/xlsx.js", "underscore.js", "XLSXConverter.js");
-/*
-var fs = require('fs');
-require("./underscore.js");
-require("./js-xlsx/jszip.js");
-require("./js-xlsx/xlsx.js");
-require("./XLSXConverter.js");
-var system = require('system');
-
-var page = require('webpage').create();
-page.includeJS("./js-xlsx/xlsx.js", function() {
-  //console.log(XLSX);
-});
-var read_stream = fs.open(system.args[1], 'rb');
-var contents = read_stream.read();
-//var xlsx_contents = xlsxNS.XLSX.read(contents, {type: 'binary'});
-console.log(contents);
-*/
-/*
-  var buffer = new Buffer(100);
-  fs.read(fd, buffer, 0, 100, 0, function(err, num) {
-    console.log(buffer.toString('utf-8', 0, num));
-  });
-  */
+// ./phantomjs convertPhantom.js [xlsx form] > [form def]
 var fs = require('fs');
 var system = require('system');
 var read_stream = fs.open(system.args[1], 'rb');
@@ -29,7 +6,6 @@ var contents = read_stream.read();
 
 var page = require('webpage').create();
 page.open("http://uw-ictd.github.io/XLSXConverter/", function() {
-  var foo = 42;
   var jsonString = page.evaluate(function(contents) {
     var xlsx = XLSX.read(contents, {type: 'binary'});
     var jsonWorkbook = to_json(xlsx);
