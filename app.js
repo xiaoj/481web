a62c39f0042ac914342dfa959b59dc5a6756b318
Tobias Kahan <tobyk100@gmail.com>
2013-06-03 12:19:57 -0700

    added some feedback that app is hosted

--- app.js
+++ app.js
@@ -6,3 +6,6 @@ app.use(express.static(__dirname + "/default"));
 app.use(express.static(__dirname));
 
 app.listen(process.env.PORT || 8888);
+
+console.log("Neonatal app is running, open browser and navigate to localhost:8888");
+console.log("Leave this terminal running.");
